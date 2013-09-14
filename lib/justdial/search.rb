007a894583c2e19825c371f84220927b36d5f0fe
CodeMangler <hsdpal@gmail.com>
2013-09-14 22:26:32 +0530

    Minor fix

--- lib/justdial/search.rb
+++ lib/justdial/search.rb
@@ -11,7 +11,8 @@ module JustDial
       valid_params = params.select { |key| [:query, :city, :area].include?(key) } # TODO: Implement Hash.slice
       geolocation_params = {geolocation: "#{params[:latitude]},#{params[:longitude]}"}
       request_params = @auth_tokens.merge(valid_params).merge(geolocation_params)
-      Provider.parse(RestClient.get(@url, request_params))
+      response = RestClient.get(@url, request_params)
+      Provider.parse(response.to_s)
     end
   end
 end
